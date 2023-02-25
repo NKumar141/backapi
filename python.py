ec82817f83d9dcb9a04dd03694b67b1e653c81c0
Nikhil <nk110700@gmail.com>
2023-02-25 23:16:10 +0530

    ddds

--- python.py
+++ python.py
@@ -9,11 +9,11 @@ import json
 # print(sys.argv[0])
 # print(sys.argv[1])
 #with open('data.json','a')as file: json.dump("",file)
-#query=sys.argv[1]
+query=sys.argv[1]
 a=[]
 # to search
 # query = sys.argv[1]
-query = "wwe";
+
 for j in search(query, tld="co.in", num=10, stop=10, pause=2):
 	print(j)
 	a.append(j)
