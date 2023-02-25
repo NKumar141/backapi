1351c56e0a0888c5998c070561a700cc322cdb10
Nikhil <nk110700@gmail.com>
2023-02-25 23:09:30 +0530

    hhhhhh

--- python.py
+++ python.py
@@ -2,20 +2,19 @@ import sys
 import os
 
 os.system('pip install google')
-os.system('pip install googlesearch-python')
+
 os.system('pip install beautifulsoup4')
 from googlesearch import search
 import json
-print(sys.argv[0])
-print(sys.argv[1])
+# print(sys.argv[0])
+# print(sys.argv[1])
 #with open('data.json','a')as file: json.dump("",file)
 #query=sys.argv[1]
 a=[]
 # to search
-query = sys.argv[1]
-
+# query = sys.argv[1]
+query = "wwe";
 for j in search(query, tld="co.in", num=10, stop=10, pause=2):
 	print(j)
-	a.append(j)
 	
 with open('data.json','w')as file: json.dump(a,file)
