73660e2ae4ef0e7a7b038bb321ee610be51fc9a5
Nikhil <nk110700@gmail.com>
2023-02-25 22:04:09 +0530

    yes

--- python.py
+++ python.py
@@ -2,6 +2,8 @@ import sys
 import os
 
 os.system('python3 -m pip install google')
+os.system('pip install googlesearch-python')
+os.system('pip install beautifulsoup4')
 from googlesearch import search
 import json
 print(sys.argv[0])
